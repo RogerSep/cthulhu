ca57a538a94c888430aac77617f4caf30dc75265
Roger Sepúlveda <roger.planetgreen@gmail.com>
2015-04-19 19:54:18 -0500

    Little proof of concept

--- webapp/public/javascripts/hello.js
+++ webapp/public/javascripts/hello.js
@@ -1,3 +1,25 @@
-if (window.console) {
-  console.log("Welcome to your Play application's JavaScript!");
-}
+angular.module("app", ["ui.router"])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+      .state("start", {
+        url: "/",
+        templateUrl: "/assets/partials/start.html"
+      });
+  })
+  .filter("markdown", function ($sce) {
+    var md = new Remarkable('full', {
+      html: false,
+      xhtmlOut: false,
+      breaks: false,
+      langPrefix: 'language-',
+      linkify: true,
+      typographer: false,
+      quotes: '“”‘’'
+    });
+
+    return function (input) {
+      return $sce.trustAsHtml(md.render(input));
+    };
+  })
