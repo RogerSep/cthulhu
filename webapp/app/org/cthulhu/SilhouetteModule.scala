b9eac710f7194a8ed17a0e118843fa5712877159
Roger Sepulveda <roger.planetgreen@gmail.com>
2015-05-18 17:24:55 -0500

    Stripped the silhouette seed project into this project to support the Google+ oauth2 sign in method.

---
+++ webapp/app/org/cthulhu/SilhouetteModule.scala
@@ -0,0 +1,169 @@
+package org.cthulhu.oauth2
+
+import com.google.inject.{ AbstractModule, Provides }
+import com.mohiva.play.silhouette.api.services._
+import com.mohiva.play.silhouette.api.util._
+import com.mohiva.play.silhouette.api.{ Environment, EventBus }
+import com.mohiva.play.silhouette.impl.authenticators._
+import com.mohiva.play.silhouette.impl.daos.DelegableAuthInfoDAO
+import com.mohiva.play.silhouette.impl.providers._
+import com.mohiva.play.silhouette.impl.providers.oauth1._
+import com.mohiva.play.silhouette.impl.providers.oauth1.secrets.{ CookieSecretSettings, CookieSecretProvider }
+import com.mohiva.play.silhouette.impl.providers.oauth1.services.PlayOAuth1Service
+import com.mohiva.play.silhouette.impl.providers.oauth2._
+import com.mohiva.play.silhouette.impl.providers.oauth2.state.{ DummyStateProvider, CookieStateProvider, CookieStateSettings }
+import com.mohiva.play.silhouette.impl.providers.openid.YahooProvider
+import com.mohiva.play.silhouette.impl.providers.openid.services.PlayOpenIDService
+import com.mohiva.play.silhouette.impl.services._
+import com.mohiva.play.silhouette.impl.util._
+import org.cthulhu.oauth2.user._
+import net.codingwell.scalaguice.ScalaModule
+import play.api.Play
+import play.api.Play.current
+
+/**
+ * The Guice module which wires all Silhouette dependencies.
+ */
+class SilhouetteModule extends AbstractModule with ScalaModule {
+
+  /**
+   * Configures the module.
+   */
+  def configure() {
+    bind[UserService].to[UserServiceImpl]
+    bind[UserDAO].to[UserDAOImpl]
+    bind[DelegableAuthInfoDAO[OAuth2Info]].to[OAuth2InfoDAO]
+    bind[CacheLayer].to[PlayCacheLayer]
+    bind[HTTPLayer].to[PlayHTTPLayer]
+    bind[IDGenerator].toInstance(new SecureRandomIDGenerator())
+    bind[PasswordHasher].toInstance(new BCryptPasswordHasher)
+    bind[FingerprintGenerator].toInstance(new DefaultFingerprintGenerator(false))
+    bind[EventBus].toInstance(EventBus())
+  }
+
+  /**
+   * Provides the Silhouette environment.
+   *
+   * @param userService The user service implementation.
+   * @param authenticatorService The authentication service implementation.
+   * @param eventBus The event bus instance.
+   * @return The Silhouette environment.
+   */
+  @Provides
+  def provideEnvironment(
+    userService: UserService,
+    authenticatorService: AuthenticatorService[SessionAuthenticator],
+    eventBus: EventBus): Environment[User, SessionAuthenticator] = {
+
+    Environment[User, SessionAuthenticator](
+      userService,
+      authenticatorService,
+      Seq(),
+      eventBus
+    )
+  }
+
+  @Provides
+  def provideSocialProviderRegistry(
+    googleProvider: GoogleProvider
+  ): SocialProviderRegistry = {
+
+    SocialProviderRegistry(Seq(
+      googleProvider
+    ))
+  }
+
+  /**
+   * Provides the authenticator service.
+   *
+   * @param fingerprintGenerator The fingerprint generator implementation.
+   * @return The authenticator service.
+   */
+  @Provides
+  def provideAuthenticatorService(
+    fingerprintGenerator: FingerprintGenerator): AuthenticatorService[SessionAuthenticator] = {
+    new SessionAuthenticatorService(SessionAuthenticatorSettings(
+      sessionKey = Play.configuration.getString("silhouette.authenticator.sessionKey").get,
+      encryptAuthenticator = Play.configuration.getBoolean("silhouette.authenticator.encryptAuthenticator").get,
+      useFingerprinting = Play.configuration.getBoolean("silhouette.authenticator.useFingerprinting").get,
+      authenticatorIdleTimeout = Play.configuration.getInt("silhouette.authenticator.authenticatorIdleTimeout"),
+      authenticatorExpiry = Play.configuration.getInt("silhouette.authenticator.authenticatorExpiry").get
+    ), fingerprintGenerator, Clock())
+  }
+
+  /**
+   * Provides the auth info service.
+   *
+   * @param passwordInfoDAO The implementation of the delegable password auth info DAO.
+   * @param oauth1InfoDAO The implementation of the delegable OAuth1 auth info DAO.
+   * @param oauth2InfoDAO The implementation of the delegable OAuth2 auth info DAO.
+   * @param openIDInfoDAO The implementation of the delegable OpenID auth info DAO.
+   * @return The auth info service instance.
+   */
+  @Provides
+  def provideAuthInfoService(
+    oauth2InfoDAO: DelegableAuthInfoDAO[OAuth2Info]): AuthInfoService = {
+
+    new DelegableAuthInfoService(oauth2InfoDAO)
+  }
+
+  /**
+   * Provides the avatar service.
+   *
+   * @param httpLayer The HTTP layer implementation.
+   * @return The avatar service implementation.
+   */
+  @Provides
+  def provideAvatarService(httpLayer: HTTPLayer): AvatarService = new GravatarService(httpLayer)
+
+  /**
+   * Provides the OAuth2 state provider.
+   *
+   * @param idGenerator The ID generator implementation.
+   * @return The OAuth2 state provider implementation.
+   */
+  @Provides
+  def provideOAuth2StateProvider(idGenerator: IDGenerator): OAuth2StateProvider = {
+    new CookieStateProvider(CookieStateSettings(
+      cookieName = Play.configuration.getString("silhouette.oauth2StateProvider.cookieName").get,
+      cookiePath = Play.configuration.getString("silhouette.oauth2StateProvider.cookiePath").get,
+      cookieDomain = Play.configuration.getString("silhouette.oauth2StateProvider.cookieDomain"),
+      secureCookie = Play.configuration.getBoolean("silhouette.oauth2StateProvider.secureCookie").get,
+      httpOnlyCookie = Play.configuration.getBoolean("silhouette.oauth2StateProvider.httpOnlyCookie").get,
+      expirationTime = Play.configuration.getInt("silhouette.oauth2StateProvider.expirationTime").get
+    ), idGenerator, Clock())
+  }
+
+  /**
+   * Provides the credentials provider.
+   *
+   * @param authInfoService The auth info service implemenetation.
+   * @param passwordHasher The default password hasher implementation.
+   * @return The credentials provider.
+   */
+  @Provides
+  def provideCredentialsProvider(
+    authInfoService: AuthInfoService,
+    passwordHasher: PasswordHasher): CredentialsProvider = {
+
+    new CredentialsProvider(authInfoService, passwordHasher, Seq(passwordHasher))
+  }
+
+  /**
+   * Provides the Google provider.
+   *
+   * @param httpLayer The HTTP layer implementation.
+   * @param stateProvider The OAuth2 state provider implementation.
+   * @return The Google provider.
+   */
+  @Provides
+  def provideGoogleProvider(httpLayer: HTTPLayer, stateProvider: OAuth2StateProvider): GoogleProvider = {
+    GoogleProvider(httpLayer, stateProvider, OAuth2Settings(
+      authorizationURL = Play.configuration.getString("silhouette.google.authorizationURL"),
+      accessTokenURL = Play.configuration.getString("silhouette.google.accessTokenURL").get,
+      redirectURL = Play.configuration.getString("silhouette.google.redirectURL").get,
+      clientID = Play.configuration.getString("silhouette.google.clientID").getOrElse(""),
+      clientSecret = Play.configuration.getString("silhouette.google.clientSecret").getOrElse(""),
+      scope = Play.configuration.getString("silhouette.google.scope")))
+  }
+}
--- webapp/build.sbt
+++ webapp/build.sbt
@@ -4,11 +4,24 @@ version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
-scalaVersion := "2.11.1"
+scalaVersion := "2.11.6"
+
+resolvers := ("Atlassian Releases" at "https://maven.atlassian.com/public/") +: resolvers.value
+
+resolvers += Resolver.sonatypeRepo("snapshots")
 
 libraryDependencies ++= Seq(
-  jdbc,
-  anorm,
   cache,
   ws
 )
+
+libraryDependencies += "com.sksamuel.elastic4s" %% "elastic4s" % "1.5.6"
+
+libraryDependencies ++= Seq(
+  "com.mohiva" %% "play-silhouette" % "2.1-SNAPSHOT",
+  "com.mohiva" %% "play-silhouette-testkit" % "2.1-SNAPSHOT" % "test"
+)
+
+libraryDependencies += "javax.inject" % "javax.inject" % "1"
+
+libraryDependencies += "net.codingwell" %% "scala-guice" % "4.0.0-beta5"
