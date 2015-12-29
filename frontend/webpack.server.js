6cf1aa4ebbeca4fc071c21776e61452c6b3f1c7b
Roger Sepulveda <roger.planetgreen@gmail.com>
2015-12-29 15:46:30 -0500

    Removed unnecessary file

--- frontend/webpack.server.js
+++
@@ -1,9 +0,0 @@
-const WebpackDevServer = require('webpack-dev-server');
-const webpack = require('webpack');
-const config = require('./webpack.config.js');
-
-const server = new WebpackDevServer(webpack(config), {
-  proxy: {
-    '*': 'http://localhost:9000'
-  }
-});
