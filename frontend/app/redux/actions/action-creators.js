4007b38c109c05d1066ed67305bcd10c886cf3b7
Roger Sepulveda <roger.planetgreen@gmail.com>
2016-01-06 21:00:54 -0500

    Added configuration file for local running.

--- frontend/app/redux/actions/action-creators.js
+++ frontend/app/redux/actions/action-creators.js
@@ -1,21 +1,20 @@
 import fetch from 'isomorphic-fetch';
 
 export const FETCH_PROJECTS = 'FETCH_PROJECTS';
-export const fetchProjects = () => ({ type: FETCH_PROJECTS });
+export function fetchProjects() {
+  return dispatch => {
+    dispatch({ type: FETCH_PROJECTS });
+
+    return fetch('/drive/projects', {
+      credentials: 'same-origin'
+    })
+      .then(response => response.json())
+      .then(json => dispatch(successProjects(json)));
+  }
+};
 
 export const SUCCESS_PROJECTS = 'SUCCESS_PROJECTS';
 export const successProjects = data => ({ type: SUCCESS_PROJECTS, data });
 
 export const FAILURE_PROJECTS = 'FAILURE_PROJECTS';
 // TODO: Add failureProjects action creator
-
-export const fetchProjects = () => dispatch => {
-    dispatch(fetchProjects());
-
-    return fetch('/drive/projects', {
-      credentials: 'same-origin'
-    })
-      .then(response => response.json())
-      .then(json => dispatch(successProjects(data)));
-  }
-};
--- webapp/app/org/cthulhu/service/google/drive/GoogleDriveServiceImpl.scala
+++ webapp/app/org/cthulhu/service/google/drive/GoogleDriveServiceImpl.scala
@@ -1,11 +1,11 @@
 package org.cthulhu.service.google.drive
 
-import java.util.Collections
-
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.services.drive.{Drive, DriveScopes}
+import play.api.Play
+import play.api.Play.current
 
 /**
   * Created by rogersepulveda on 12/18/15.
@@ -16,7 +16,7 @@ class GoogleDriveServiceImpl extends GoogleDriveService {
     .setTransport(Constants.HttpTransport)
     .setJsonFactory(Constants.JsonFactory)
     .setServiceAccountId("p12-test@focus-fold-94417.iam.gserviceaccount.com")
-    .setServiceAccountPrivateKeyFromP12File(new java.io.File("/Users/rogersepulveda/apl/cthulhu/webapp/conf/fun-notes.p12"))
+    .setServiceAccountPrivateKeyFromP12File(new java.io.File(Play.application.configuration.getString("com.cthulhu.drive.p12file").get))
     .setServiceAccountScopes(DriveScopes.all())
     .build()
 
