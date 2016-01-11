b84bc3a545980c42545f60c60c185722f36449e4
Camilo Orrego <acornssn@gmail.com>
2016-01-11 18:07:22 -0500

    Refactor redux/reducers/projects

--- frontend/app/redux/reducers/projects.js
+++ frontend/app/redux/reducers/projects.js
@@ -7,35 +7,43 @@ import {
   FETCH_ITEMS
 } from '../actions/action-creators';
 
-export default (state = {}, action) => {
+const initialState = {
+  isFetching: false,
+  projects: [],
+  items: []
+};
+
+export default (state = initialState, action) => {
   switch(action.type) {
     case FETCH_PROJECTS:
+
     case CREATE_PROJECT:
+
     case FETCH_ITEMS:
       return {
         ...state,
         isFetching: true
       };
-      break;
+
     case SUCCESS_PROJECTS:
       return {
         ...state,
         isFetching: false,
-        projects: action.data
+        projects: [...action.data]
       };
-      break;
+
     case PROJECT_CREATION:
       return {
         ...state,
         isFetching: false,
-        projects: (state.projects || []).concat(action.data)
+        projects: [...state.projects, ...action.data]
       };
-      break;
+
     case ITEMS_FETCHED:
       return {
         ...state,
         isFetching: false,
-        items: (state.projects || []).map(project => {
+        items: state.projects.map(project => {
           if (project.id === action.parentId) {
             return {
               ...project,
@@ -46,7 +54,7 @@ export default (state = {}, action) => {
           }
         })
       };
-      break;
+
     default:
       return state;
   }
