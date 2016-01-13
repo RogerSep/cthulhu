3f863301f1007e39f9a1720dcd58b92b81f26443
Camilo Orrego <acornssn@gmail.com>
2016-01-13 17:09:52 -0500

    Add CollaborativeItem.spec.js

--- frontend/app/components/presentational/CollaborativeItem/CollaborativeItem.spec.js
+++ frontend/app/components/presentational/CollaborativeItem/CollaborativeItem.spec.js
@@ -1,8 +1,24 @@
+import sd from 'skin-deep';
+import React from 'react';
 import CollaborativeItem from './CollaborativeItem';
 import { expect } from 'chai';
 
 describe('CollaborativeItem component', () => {
-  it('should be tested right now!', () => {
-    expect(true).to.equal(true);
+  let tree;
+  const props = {
+    name: 'foo',
+    link: '#foo'
+  };
+
+  beforeEach(() => {
+    tree = sd.shallowRender(React.createElement(CollaborativeItem, props));
+  });
+
+  it('should have a prop link bind to Link subtree', () => {
+    expect(tree.subTree('Link').props.to).to.equal(props.link);
+  });
+
+  it('should have a prop name as inner text', () => {
+    expect(tree.subTree('div').text()).to.equal(props.name);
   });
 });
