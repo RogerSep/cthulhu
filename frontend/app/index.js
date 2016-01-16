e39812d24449646bb96ceb761e201126308855dc
Roger Sepulveda <roger.planetgreen@gmail.com>
2016-01-16 18:23:43 -0500

    Changed the usage of the router because of the version update.

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -3,28 +3,28 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { Router, Route, IndexRoute } from 'react-router';
-import { createHistory } from 'history';
-import { syncReduxAndRouter, routeReducer } from 'redux-simple-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { syncHistory, routeReducer } from 'redux-simple-router';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
 import { Root, ProjectGrid, Document } from './components';
 import reducers from './redux/reducers/reducers';
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware,
+console.log(browserHistory);
 
-  // createLogger()
-)(createStore);
-
+const reduxRouterMiddleware = syncHistory(browserHistory);
 const reducer = combineReducers(Object.assign({}, reducers, {
   routing: routeReducer
 }));
 
-const store = createStoreWithMiddleware(reducer);
-const history = createHistory();
+const createStoreWithMiddleware = applyMiddleware(
+  reduxRouterMiddleware,
+  thunkMiddleware
 
-syncReduxAndRouter(history, store);
+  // createLogger()
+)(createStore);
+
+const store = createStoreWithMiddleware(reducer);
 
 const app = document.createElement('div');
 document.body.appendChild(app);
@@ -33,7 +33,7 @@ window.gapi.load('auth:client,drive-realtime');
 
 render(
   <Provider store={store}>
-    <Router history={history}>
+    <Router history={browserHistory}>
       <Route path="/" component={Root}>
         <IndexRoute component={ProjectGrid} />
         <Route path="projects/:projectId" component={Document} />
