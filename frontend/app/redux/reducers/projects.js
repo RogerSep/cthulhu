8a188dede57e90cd0776beff6e486aa9dd1b76a2
David Gomez <dg.dave@gmail.com>
2016-01-17 17:26:15 -0500

    fix indents

--- frontend/app/redux/reducers/projects.js
+++ frontend/app/redux/reducers/projects.js
@@ -19,66 +19,66 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_PROJECTS:
+  case FETCH_PROJECTS:
 
-    case CREATE_PROJECT:
+  case CREATE_PROJECT:
 
-    case FETCH_ITEMS:
-      return {
-        ...state,
-        isFetching: true
-      };
+  case FETCH_ITEMS:
+    return {
+      ...state,
+      isFetching: true
+    };
 
-    case SUCCESS_PROJECTS:
-      return {
-        ...state,
-        isFetching: false,
-        projects: [...action.data]
-      };
+  case SUCCESS_PROJECTS:
+    return {
+      ...state,
+      isFetching: false,
+      projects: [...action.data]
+    };
 
-    case PROJECT_CREATION:
-      return {
-        ...state,
-        isFetching: false,
-        projects: [...state.projects, ...action.data]
-      };
+  case PROJECT_CREATION:
+    return {
+      ...state,
+      isFetching: false,
+      projects: [...state.projects, ...action.data]
+    };
 
-    case UPDATE_MODEL:
-      const model = modelConversions(action.model);
-      const { editing } = state.model || { editing: [] };
-      return {
-        ...state,
-        model: {
-          ...model,
-          editing
-        }
-      };
+  case UPDATE_MODEL:
+    const model = modelConversions(action.model);
+    const { editing } = state.model || { editing: [] };
+    return {
+      ...state,
+      model: {
+        ...model,
+        editing
+      }
+    };
 
-    case EDIT_COLLABORATIVE_OBJECT:
-      return {
-        ...state,
-        model: {
-          ...state.model,
-          editing: state.model.editing.filter(id => id !== action.objectId).concat(action.objectId)
-        }
-      };
+  case EDIT_COLLABORATIVE_OBJECT:
+    return {
+      ...state,
+      model: {
+        ...state.model,
+        editing: state.model.editing.filter(id => id !== action.objectId).concat(action.objectId)
+      }
+    };
 
-    case FINISH_EDIT_COLLABORATIVE_OBJECT:
-      return {
-        ...state,
-        model: {
-          ...state.model,
-          editing: state.model.editing.filter(id => id !== action.objectId)
-        }
-      };
+  case FINISH_EDIT_COLLABORATIVE_OBJECT:
+    return {
+      ...state,
+      model: {
+        ...state.model,
+        editing: state.model.editing.filter(id => id !== action.objectId)
+      }
+    };
 
-    case ERROR:
-      return {
-        ...state,
-        error: action.error
-      };
+  case ERROR:
+    return {
+      ...state,
+      error: action.error
+    };
 
-    default:
-      return state;
+  default:
+    return state;
   }
 };
